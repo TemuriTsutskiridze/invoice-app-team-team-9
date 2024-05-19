03a695c91b179d68e1acf7bd1f349c6375661571
gkhuskivadze <g.khuskivadze@gmail.com>
2024-05-19 15:33:53 +0400

    add interactivity  to edit-cancel invoice

--- pages/invoice/invoice.js
+++ pages/invoice/invoice.js
@@ -19,17 +19,21 @@ let total = document.querySelectorAll(".total");
 let orderSummaryBox = document.querySelectorAll(".orderSummaryBox");
 let orderTotalBox = document.querySelector(".orderTotalBox");
 let buttonsContainer2 = document.querySelector(".buttonsContainer2");
+let buttonsBox2 = document.querySelector(".buttonsBox2");
+let buttonsBox = document.querySelector(".buttonsBox");
+
+//for dark mode functionality
 moon.addEventListener("click", () => {
   input.forEach((el) => el.classList.toggle("input-dark-mode"));
   label.forEach((el) => el.classList.toggle("label-dark-mode"));
   main.classList.toggle("dark-mode");
   addNewItem.classList.toggle("dark-mode");
   addNewItem.classList.toggle("dark-mode-dark-blue");
-  addNewItem.style.backgroundColor = "#252945";
+  addNewItem.classList.toggle("input-dark-mode-bgc");
   linearBoxDiv.classList.toggle("dark-mode");
   linearBoxDiv.style.border = "0";
   buttonsContainer.classList.toggle("invoice-dark-mode");
-  cancelBtn.style.backgroundColor = "#252945";
+  cancelBtn.classList.toggle("input-dark-mode-bgc");
   invoiceView.classList.toggle("dark-mode");
   statusBox.classList.toggle("invoice-dark-mode");
   invoiceSummary.classList.toggle("invoice-dark-mode");
@@ -44,4 +48,27 @@ moon.addEventListener("click", () => {
   clientEmail.classList.toggle("dark-mode-white");
   orderTotalBox.classList.toggle("dark-mode-dark");
   buttonsContainer2.classList.toggle("invoice-dark-mode");
+  moon;
+});
+
+//for edit functionality
+
+let editBtn = document.querySelector(".editBtn");
+let deleteBtn = document.querySelector(".deleteBtn");
+let paidBtn = document.querySelector(".paidBtn");
+
+editBtn.addEventListener("click", () => {
+  invoiceView.classList.replace("displayBlock", "displayNone");
+  buttonsBox2.classList.replace("displayBlock", "displayNone");
+  main.classList.replace("displayNone", "displayBlock");
+  buttonsBox.classList.replace("displayNone", "displayBlock");
+  linearBoxDiv.classList.replace("displayNone", "displayBlock");
+});
+
+cancelBtn.addEventListener("click", () => {
+  invoiceView.classList.replace("displayNone", "displayBlock");
+  buttonsBox2.classList.replace("displayNone", "displayBlock");
+  main.classList.replace("displayBlock", "displayNone");
+  buttonsBox.classList.replace("displayBlock", "displayNone");
+  linearBoxDiv.classList.replace("displayBlock", "displayNone");
 });
