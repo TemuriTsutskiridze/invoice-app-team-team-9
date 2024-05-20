9c6a542eede2af287e4c12df046f6701e76c1fc2
NikaApts1auri <nika.aptsiauri77@gmail.com>
2024-05-20 21:20:01 +0400

    wip on api

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// async function colab(){
-//     const userResponse= await fetch("https://app.swaggerhub.com/apis-docs/SergoAzizbekyan/invoice-api/v1"),{
-//         method:"POST",
-//         headers: {
-//             "Content-Type":"application/json"
-//         },
-//         body{
+async function colab(){
+    const userResponse= await fetch("https://invoiceapi-rpgn.onrender.com/api/"),{
+        method:"POST",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body{
 
 
-//         }
-//     }
-// }
+        }
+    }
+}
