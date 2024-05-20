f8f4c2a7b0f2c77de62c1044670774d01bb4c3c7
NikaApts1auri <nika.aptsiauri77@gmail.com>
2024-05-20 14:55:56 +0400

    WIP json

---
+++ script.js
@@ -0,0 +1,12 @@
+async function colab(){
+    const userResponse= await fetch("https://app.swaggerhub.com/apis-docs/SergoAzizbekyan/invoice-api/v1"),{
+        method:"POST",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body{
+
+
+        }
+    }
+}
